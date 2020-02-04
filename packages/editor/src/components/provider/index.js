3be73cce14b1b551e9dfc6f93da36188b4dc1a40
Andrés <andres.maneiro@automattic.com>
2020-02-04 18:59:43 +0100

    Consolidate block settings names

--- packages/editor/src/components/provider/index.js
+++ packages/editor/src/components/provider/index.js
@@ -119,6 +119,8 @@ class EditorProvider extends Component {
 				'__experimentalEnableFullSiteEditing',
 				'__experimentalEnableFullSiteEditingDemo',
 				'__experimentalEnablePageTemplates',
+				'__experimentalGlobalStylesUserEntityID',
+				'__experimentalGlobalStylesBase',
 				'showInserterHelpPanel',
 				'gradients',
 			] ),
